d565ebc64d847273b4e1244efe2d108cdb1dcd06
mvlinh <linh.mv184132@sis.hust.edu.vn>
2022-06-05 07:58:18 +0000

    change todo status

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,22 @@
+import React from 'react'
 /* 
   【TodoItemコンポーネント】
 　・Todoアイテムを表示する
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
-
-function TodoItem( {item} ) {
+function TodoItem({item}) {
+  const [state,setState] =React.useState(false);
+  const getClickBox = () => {
+    setState(!state);
+  }
+   console.log(state);
   return (
-    <label className="panel-block">
-      <input type="checkbox" />
+      <label className= {state ? "panel-block has-text-grey-light" : "panel-block"}>
+      <input type = "checkbox" onClick={() => getClickBox()} />
       {item.text}
     </label>
   );
 }
+
 export default TodoItem;
